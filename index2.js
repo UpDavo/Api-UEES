c4abf68c2f49fbf2103cd0ed85ce919dc21218f6
Anthony Villegas <advillegas@uees.edu.ec>
2021-03-26 12:19:36 -0500

    Funcion de get email

--- index2.js
+++ index2.js
@@ -3,7 +3,6 @@ const bodyParser = require("body-parser");
 const https = require("https");
 const fetch = require("node-fetch");
 const request = require("request");
-var cors = require('cors')
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 const port = process.env.PORT || 3000;
@@ -12,15 +11,44 @@ const httpsAgent = new https.Agent({
 });
 
 const APIWOLKBOX = "https://34.86.151.171/ipdialbox/api_campaing.php";
-const APICRMWOLKVOX = "https://crm.ipdialbox.com/server/API/query.php";
 const TOKEN = "7b69645f6469737472697d2d3230323031313234313531363039";
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
+app.get("/main", (req, res) => {
   return res.send("Hola esta es la api");
+});
+
+app.post("/campana", (req, res) => {
+  let url = new URL(APIWOLKBOX);
+  const params = {
+    token: TOKEN,
+    nombre: req.body.nombre,
+    correo: req.body.correo,
+    celular: req.body.celular,
+    comentario: req.body.comentario,
+    interesado: req.body.interesado,
+    id: req.body.id,
+    origen: req.body.origen,
+    campana: req.body.campana,
+  };
+  Object.keys(params).forEach((key) =>
+    url.searchParams.append(key, params[key])
+  );
+  console.log("/n");
+  console.log(url.href);
+
+  fetch(url, {
+    agent: httpsAgent,
+  })
+    .then((response) => {
+      return res.send(response);
+    })
+    .catch((error) => {
+      return res.send(error);
+    });
 });
 
 app.post("/campana2", (req, res) => {
@@ -66,40 +94,31 @@ app.post("/campana2", (req, res) => {
   request(options, callback);
 });
 
-
-//Analisis de usuarios
-app.post("/checkuser/:email", cors(), (req, res) => {
-  var myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/json");
-  myHeaders.append("Cookie", "PHPSESSID=acl0oi51f1s3a7du7h4i5s8e9s");
-
-  console.log(req.params.email)
-
-  var dataCrm = JSON.stringify({
-    nit: "uees",
-    modulo: "Estudiantes",
-    campo: "EMAIL",
-    valor: "NA@NA.COM",
-  });
-
-  //options to crm
-  let requestOptionsSearchCRM = {
-    headers: myHeaders,
-    method: "POST",
-    redirect: "manual",
-    body: dataCrm,
+app.post("/test", (req, res) => {
+  const data = {
+    nombre: req.body["contact[first_name]"],
+    apellido: req.body["contact[last_name]"],
+    telefono: req.body["contact[phone]"],
+    email: req.body["contact[email]"],
   };
-
-  let checkEmail = await fetch(APICRMWOLKVOX, requestOptionsSearchCRM);
-  let respuesta = await checkEmail.text();
-
-  console.log(respuesta)
-  res.send(respuesta)
+  console.log(req.body);
+  return res.send("Los datos fueron enviados");
 });
-
-
 
 //Puerto del servidor
 app.listen(port, () => {
   console.log(`Servidor iniciado en el puerto ${port}...`);
 });
+
+data = {
+  "contact[id]": "1358",
+  "contact[email]": "ab.cesarcuadradollandan@outlook.com",
+  "contact[first_name]": "Cesar",
+  "contact[last_name]": "Cuadrado",
+  "contact[phone]": "593986523796",
+  "contact[orgname]": "",
+  "contact[customer_acct_name]": "",
+  "contact[tags]": "Externos",
+  "contact[ip4]": "127.0.0.1",
+  seriesid: "9",
+};
